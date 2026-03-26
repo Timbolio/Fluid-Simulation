4ae45f3f2e3ac32456cbbdc484a9c94f543d6228
Timbolio <159907642+Timbolio@users.noreply.github.com>
2026-03-26 19:57:53 +0000

    Created empty unity project and added blank particle script for fluid

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    // This script will contain the behaviour of each particle of fluid (droplet), and will run calculations for density, pressure, and viscosity forces. It will also handle the movement of the particle based on these forces.
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
