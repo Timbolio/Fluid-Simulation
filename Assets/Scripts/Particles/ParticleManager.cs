161cdfa091f41aaaa65442052ec5e4713e64ca9c
Timbolio <159907642+Timbolio@users.noreply.github.com>
2026-03-26 20:35:25 +0000

    Replaced gizmo with sprite, lowered to one single particle. Decreased size.

--- Assets/Scripts/Particles/ParticleManager.cs
+++ Assets/Scripts/Particles/ParticleManager.cs
@@ -11,54 +11,39 @@ public class ParticleManager : MonoBehaviour
         public Vector2 velocity;
     }
 
-    List<Particle> particles = new List<Particle>();
+    Particle particle;
 
-    // Settings
-    public int particleCount = 200;
-    public float spawnRadius = 3f;
+    [Header("Rendering")]
+    public GameObject particlePrefab;
+    GameObject particleVisual;
+
+    [Header("Physics")]
     public float gravity = -9.8f;
-    public float particleSize = 0.2f;
 
     void Start()
     {
-        particles = new List<Particle>();
-        for (int i = 0; i < particleCount; i++)
+        particle = new Particle
         {
-            Particle p = new Particle
-            {
-                position = Random.insideUnitCircle * spawnRadius,
-                velocity = Vector2.zero
-            };
-            particles.Add(p);
-        }
+            position = Vector2.zero,
+            velocity = Vector2.zero
+        };
+
+        particleVisual = Instantiate(particlePrefab, particle.position, Quaternion.identity);
     }
 
     
     void Update()
     {
         float dt = Time.deltaTime;
-        for (int i = 0; i < particles.Count; i++)
-        {
-            Particle p = particles[i];
+        
+        // Apply gravity
+        particle.velocity += new Vector2(0, gravity) * dt;
+        // Update position
+        particle.position += particle.velocity * dt;
+        // Sync
+        particleVisual.transform.position = particle.position;
 
-            // Apply gravity
-            p.velocity += new Vector2(0, gravity) * dt;
-            // Update position
-            p.position += p.velocity * dt;
-            // Save Back
-            particles[i] = p;
-        }
+
     }
 
-    void OnDrawGizmos()
-    {
-        if (particles == null) return;
-
-        Gizmos.color = Color.cyan;
-
-        foreach (var p in particles)
-        {
-            Gizmos.DrawSphere(p.position, particleSize);
-        }
-    }
 }
