048cd0b8d605cfb54dbbe7163d36aaeb5896cb41
Timbolio <159907642+Timbolio@users.noreply.github.com>
2026-03-26 20:10:18 +0000

    Used gizmos to quickly see particles on screen, currently floating within a circle

--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -1,19 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Particle : MonoBehaviour
 {
     // This script will contain the behaviour of each particle of fluid (droplet), and will run calculations for density, pressure, and viscosity forces. It will also handle the movement of the particle based on these forces.
 
-
+    public List<Vector2> particles = new List<Vector2>();
 
     void Start()
     {
         
     }
-
-
-    void Update()
+    void OnDrawGizmos() // Starting with gizmos to visualize the particles in the editor, we will later replace this with actual rendering of the particles probably using sprites or meshes.
     {
-        
+        if (particles == null || particles.Count == 0)
+        {
+            particles = new List<Vector2>();
+
+            for (int i = 0; i < 200; i++)
+            {
+                particles.Add(Random.insideUnitCircle * 3f);
+            }
+        }
+
+        Gizmos.color = Color.cyan;
+
+        foreach (var p in particles)
+        {
+            Gizmos.DrawSphere(p, 0.05f);
+        }
     }
 }
